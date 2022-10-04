95b0744d150adfe9f14553f0d85607bbb2c9681f
Viktor Farcic <viktor@farcic.com>
2022-10-04 18:21:44 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/fibonacci", fibonacciHandler)
+	r.POST("/slack", slackHandler)
+	r.POST("/video", videoPostHandler)
+	r.GET("/videos", videosGetHandler)
+	r.GET("/", rootHandler)
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	r.Run(fmt.Sprintf(":%s", port))
+}
