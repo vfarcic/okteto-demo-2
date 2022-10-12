a29a829c37974fff397bfdbb6dacfd6d103780af
Viktor Farcic <viktor@farcic.com>
2022-10-13 01:11:02 +0400

    Added Okteto

--- root.go
+++ root.go
@@ -16,7 +16,7 @@ func rootHandler(c *gin.Context) {
 	version := os.Getenv("VERSION")
 	output := os.Getenv("MESSAGE")
 	if len(output) == 0 {
-		output = "This is a silly demo"
+		output = "Okteto?"
 	}
 	if len(version) > 0 {
 		output = fmt.Sprintf("%s version %s", output, version)
