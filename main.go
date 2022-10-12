15692827102a7b90368f09c0110b06c0b7611932
Viktor Farcic <viktor@farcic.com>
2022-10-13 00:55:29 +0400

    Initial commit

--- main.go
+++ main.go
@@ -9,10 +9,6 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/fibonacci", fibonacciHandler)
-	r.POST("/slack", slackHandler)
-	r.POST("/video", videoPostHandler)
-	r.GET("/videos", videosGetHandler)
 	r.GET("/", rootHandler)
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
